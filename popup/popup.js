220afb15c1ab731b9a589ee84e9e6c54a7acf5b3
Kumar Priyank <kpriyank88@gmail.com>
2025-01-02 10:31:19 -0500

    commit

--- popup/popup.js
+++ popup/popup.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 return;
             }
             if (data.formData) {
-                chrome.tabs.create({ url: 'https://practice.expandtesting.com/inputs' }, (newTab) => {
+                chrome.tabs.create({ url: 'https://practice.expandtesting.com/inputs' }, (newTab) => { //Change URL to target RockTalk page
                     if (chrome.runtime.lastError) {
                         console.error('Error creating new tab:', chrome.runtime.lastError);
                         return;
